ff6082f708973e12be132a502258244b53710848
GavinDN <370809296@qq.com>
2021-02-24 00:04:17 +0800

    使用函数strip()

---
+++ 删除空白.py
@@ -0,0 +1,9 @@
+# coding=gbk
+favorite_languages = " Python"
+print(favorite_languages)
+print(len(favorite_languages))
+
+# Python能够找出字符串开头和末尾多余的空白并清除
+favorite_languages = favorite_languages.strip()
+print(favorite_languages)
+print(len(favorite_languages))
