e97df82228bcd000b9f763d101433130edb1eca7
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-02-25 23:50:00 +0800

    Add files via upload

---
+++ name_case_1.py
@@ -0,0 +1,12 @@
+# coding=gbk 
+
+# 个性化消息
+'''
+将用户的姓名存到一个变量中
+并向该用户显示一条信息。
+显示的消息应非常简单
+如"Hello Gavin,would you like learn some Python today?"
+'''
+
+name = input("请输入您的姓名:")
+print("\"Hello " + name +",would you like learn some Python today?\"")
---
+++ name_case_3.py
@@ -0,0 +1,16 @@
+# coding=gbk
+
+# 名言
+'''
+找一句你钦佩的名人说的名言
+输出类似于下面这样（包括引号）
+Albert Einstein once said,"A person who never made a mistake never tried anything new"
+将名人的姓名储存在变量famous_person中
+再创建要显示的信息
+并将其存储在变量message中
+然后再打印出来
+'''
+
+famous_person = 'Thomas Edison '
+message = 'Achievement provides the only real pleasure in life.'
+print("\"" + famous_person + "said," + message + "\"")
