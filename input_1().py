7367ca7eadfea24f72f5da90ce5919a98bf5147b
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-03-03 22:28:17 +0800

    输入古诗后将其打印

---
+++ input_1().py
@@ -0,0 +1,9 @@
+# coding=gbk
+
+print("请输入你想展示给大家的一篇古诗")
+firstline = input("请输入第一行内容：")               # 输入提示
+secondline = input("请输入第二行内容：")
+thirdline = input("请输入第三行内容：")
+fourth  = input("请输入第四行内容：")
+list = [firstline, secondline, thirdline, fourth]  # 将内容存放入列表
+print(list)                                        # 输出列表内容
