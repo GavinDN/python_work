4922ff6a00f6fda8bc31e39fce99a580d976e3bd
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-04-05 20:27:35 +0800

    将字符串转换为bool类型

---
+++ bool_2().py
@@ -0,0 +1,9 @@
+# coding=gbk
+
+# 将字符串转换为bool类型
+
+print(bool(''))              # 输出：Flase
+print(bool(1))               # 输出：Flase
+print(bool('Python'))        # 输出：Ture
+print(bool('2022准时上岸'))   # 输出：Ture
+
