8b960a56e425715b83ce2ac6acea8d9aa6c35c20
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-04-30 20:14:41 -0500

    实现将二进制IP地址转换为十进制表示的IP地址

---
+++ int_4().py
@@ -0,0 +1,11 @@
+# coding=gbk
+
+# 实现将二进制IP地址转换为十进制表示的IP地址
+
+def get_num(b):
+	list_str = b.split(" ")
+	new_str = []
+	for i in list_str:
+		new_str.append(str(int(i, 2)))
+	return ".".join(new_str)
+print(get_num("00001010 00000011 00001001 00001100"))
