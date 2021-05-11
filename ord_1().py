83b2fbe065299e4a3ddc319e37ce598b5e2357d0
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-05-11 21:57:15 +0800

    将字符转换为相应的整数

---
+++ ord_1().py
@@ -0,0 +1,12 @@
+# coding=gbk
+
+# 将字符转换为相应的整数
+
+# 返回对应的整数值
+print(ord('A'))  # 输出：65
+print(ord('z'))  # 输出：122
+print(ord('我')) # 输出：25105
+print(ord('0'))  # 输出：48
+print(ord('9'))  # 输出：57
+print(ord('*'))  # 输出：42
+print(ord('①')) # 输出：9312
