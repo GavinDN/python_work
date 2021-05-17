b78e5de62f8f19d5190bde5ed3d7396497daed4b
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-05-17 22:06:04 +0800

    Add files via upload

---
+++ str_2().py
@@ -0,0 +1,9 @@
+# coding=gbk
+
+# 将列表转换为字符串形式
+
+list1=['董楠','Gavin','Python学习','2021年']
+print(list1)            # 输出列表
+print(type(list1))      # 列表类型
+print(str(list1))       # 列表转换为字符串
+print(type(str(list1))) # 字符串类型
