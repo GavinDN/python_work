cdecf86a243223651727a03712a02a9645cca668
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-05-21 08:20:48 +0800

    将集合转换为字符串形式

---
+++ str_5().py
@@ -0,0 +1,10 @@
+# coding=gbk
+
+# 将集合转换为字符串形式
+
+# 下面使用str()方法将集合转换为字符串形式，代码如下:
+
+set = {'零基础学Python', 'Python从入门到项目实践', 'Python项目开发案例集锦', 'Python编程锦囊'}
+mystr = str(set)    # 集合转换为字符串
+print(mystr)        # 输出字符串
+print(type(mystr))  # 查看类型
