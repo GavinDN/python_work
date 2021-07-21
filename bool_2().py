8f4f9a28125fa8503127a0846bdf8469ac093439
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-07-21 22:48:58 +0800

    将字符串转换为bool类型

--- bool_2().py
+++ bool_2().py
@@ -2,8 +2,8 @@
 
 # 将字符串转换为bool类型
 
-print(bool(''))             # 输出：Flase
-print(bool(1))              # 输出：Flase
-print(bool('Python'))       # 输出：Ture
-print(bool('2022准时上岸'))  # 输出：Ture
+print(bool(''))              # 输出：Flase
+print(bool(1))               # 输出：Flase
+print(bool('Python'))        # 输出：Ture
+print(bool('2022准时上岸'))   # 输出：Ture
 
