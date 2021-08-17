f93c36eedf16f82b3c19f9c403c637b23695ee65
Gavin <74776052+GavinDN@users.noreply.github.com>
2021-08-17 20:38:37 +0800

    获取复数的模

--- abs_2().py
+++ abs_2().py
@@ -4,8 +4,8 @@
 
 # 通过abs()函数获取复数的模，代码如下：
 
-print(abs(1-2j))   # 获取1-2j复数的模
-print(abs(1+5j))   # 获取1+5j复数的模
-print(abs(6*8j))   # 获取6*8j复数的模
-print(abs(-1-1j))  # 获取-1-1j复数的模
+print(abs(1-2j))    # 获取1-2j复数的模
+print(abs(1+5j))    # 获取1+5j复数的模
+print(abs(6*8j))    # 获取6*8j复数的模
+print(abs(-1-1j))   # 获取-1-1j复数的模
  
